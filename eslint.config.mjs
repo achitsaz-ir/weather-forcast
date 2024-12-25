9969b690bcff4fe06026e86b3da0e20d88ee3707
imachitsaz@gmail.com <imachitsaz@gmail.com>
2024-12-25 14:14:38 +0330

    ✨ [8] Better configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,11 @@
 import _import from 'eslint-plugin-import';
 import prettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
+import { fixupPluginRules } from '@eslint/compat';
 import nextEslintPluginNext from '@next/eslint-plugin-next';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
 export default [
     {
@@ -31,54 +19,38 @@ export default [
             react,
             '@next/next': nextEslintPluginNext
         },
-
         languageOptions: {
             parser: tsParser,
-            ecmaVersion: 8,
+            ecmaVersion: 2020,
             sourceType: 'module',
-
             parserOptions: {
-                createDefaultProgram: true,
-
                 ecmaFeatures: {
                     jsx: true
                 }
             }
         },
-
         settings: {
             react: {
                 version: 'detect'
             },
-
             'import/parsers': {
                 '@typescript-eslint/parser': ['.ts', '.tsx']
             },
-
             'import/resolver': {
                 node: {
                     extensions: ['.js', '.jsx', '.ts', '.tsx']
                 }
             }
         },
-
         rules: {
             'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-
-            'react/jsx-filename-extension': [
-                'error',
-                {
-                    extensions: ['.ts', '.tsx', '.jsx', '.js']
-                }
-            ],
-
+            'react/jsx-filename-extension': ['error', { extensions: ['.ts', '.tsx', '.jsx', '.js'] }],
             'no-param-reassign': 'error',
             'react/display-name': 'error',
             'react/no-array-index-key': 'warn',
             'no-console': 'error',
             'no-shadow': 'off',
             '@typescript-eslint/no-shadow': 'error',
-
             '@typescript-eslint/naming-convention': [
                 'error',
                 {
@@ -88,40 +60,14 @@ export default [
                     trailingUnderscore: 'allow'
                 }
             ],
-
-            'prefer-destructuring': [
-                'error',
-                {
-                    object: true,
-                    array: false
-                }
-            ],
-
-            'import/no-cycle': [
-                'error',
-                {
-                    maxDepth: 1
-                }
-            ],
-
+            'prefer-destructuring': ['error', { object: true, array: false }],
+            'import/no-cycle': ['error', { maxDepth: 1 }],
             'import/no-extraneous-dependencies': [
                 'error',
-                {
-                    devDependencies: false,
-                    optionalDependencies: false,
-                    peerDependencies: false
-                }
+                { devDependencies: false, optionalDependencies: false, peerDependencies: false }
             ],
-
             'no-restricted-imports': 'error',
-
-            '@typescript-eslint/no-unused-vars': [
-                'error',
-                {
-                    vars: 'all',
-                    args: 'none'
-                }
-            ]
+            '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'none' }]
         }
     }
 ];
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
+    autoprefixer: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'nesting-rules': true,
+      },
+    },
+    cssnano: {
+      preset: 'default',
+    },
   },
 };
 
